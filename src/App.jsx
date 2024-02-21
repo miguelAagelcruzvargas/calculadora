00be0125b0f987824e72735fccc73979915301ef
miguelAagelcruzvargas <139577769+miguelAagelcruzvargas@users.noreply.github.com>
2024-02-21 08:50:55 -0600

    Add files via upload
    v1- calculator

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>calculator</h1>
+      <hr />
+      <table className='center'>
+        <tbody>
+          <tr>
+            <td colSpan={4}>
+              11
+            </td>
+          </tr>
+          <tr>
+            <td>
+              21
+            </td>
+            <td>
+              22
+            </td>
+            <td>
+              23
+            </td>
+            <td>
+              24
+            </td>
+          </tr> <tr>
+            <td>
+              31
+            </td>
+            <td>
+              32
+            </td>
+            <td>
+              33
+            </td>
+            <td>
+              34
+            </td>
+          </tr> <tr>
+            <td>
+              41
+            </td>
+            <td>
+              42
+            </td>
+            <td>
+              43
+            </td>
+            <td>
+              44
+            </td>
+          </tr> <tr>
+            <td>
+              51
+            </td>
+            <td>
+              52
+            </td>
+            <td>
+              53
+            </td>
+            <td>
+              54
+            </td>
+          </tr> <tr>
+            <td>
+              61
+            </td>
+            <td>
+              62
+            </td>
+            <td>
+              63
+            </td>
+            <td>
+              64
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+
+  )
+}
+
+export default App
